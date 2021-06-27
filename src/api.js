658ee076ddc80429f15b7048c4f3513676854f9c
Jose Lobato <joselobatof@gmail.com>
2021-06-27 01:35:40 -0500

    coinDetails

--- src/api.js
+++ src/api.js
@@ -7,6 +7,27 @@ async function getAssets() {
   return data;
 }
 
+async function getAsset(coinName) {
+  let data = await fetch(`${url}/assets/${coinName}`);
+  data = await data.json();
+  data = data.data;
+  return data;
+}
+
+async function getAssetHistory(coinName) {
+  const now = new Date();
+  const end = now.getTime()
+  now.setDate(now.getDate()-1)
+  const start = now.getTime()
+
+  let data = await fetch(`${url}/assets/${coinName}/history?interval=h1&start=${start}&end=${end}`);
+  data = await data.json();
+  data = data.data;
+  return data;
+}
+
 export default {
   getAssets,
+  getAsset,
+  getAssetHistory
 };
--- src/router.js
+++ src/router.js
@@ -4,6 +4,7 @@ import Router from 'vue-router';
 import Home from '@/views/Home';
 import About from '@/views/About';
 import Error from '@/views/Error';
+import CoinDetail from '@/views/CoinDetail';
 
 Vue.use(Router);
 
@@ -17,6 +18,11 @@ const routes = [
     path: '/about',
     name: 'about',
     component: About,
+  },
+  {
+    path: '/coin/:id',
+    name: 'coin-detail',
+    component: CoinDetail,
   },
   {
     path: '*',
