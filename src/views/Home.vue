0f5a30bcc605760b8323d82d38dedad14fe3b947
Jose Lobato <joselobatof@gmail.com>
2021-06-26 21:20:26 -0500

    adding router

---
+++ src/views/Home.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <PxAssetsTable />
+  </div>
+</template>
+
+<script>
+import PxAssetsTable from "@/components/PxAssetsTable";
+
+export default {
+  name: "Home",
+  components: {
+    PxAssetsTable,
+  },
+};
+</script>
